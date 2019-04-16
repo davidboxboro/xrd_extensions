2f3a3a6e0bf98f76bc26159f606a0f9ba626759e
David Lu <davidboxboro@gmail.com>
2019-04-15 23:45:18 -0400

    more work

--- xrd_extensions/lin_prog_heuristic.py
+++ xrd_extensions/lin_prog_heuristic.py
@@ -4,7 +4,7 @@ from scipy.optimize import linprog
 
 g = 3
 num_users = 10
-num_serv = 100
+num_serv = 10
 num_pairs = num_users * (num_users-1) / 2
 num_var = num_pairs * num_serv
 
@@ -32,12 +32,41 @@ for i in range(1, num_serv):
         A_ele[j*num_serv+i] = -1
     A.append(A_ele)
 
+for i in range(num_pairs):
+    for j in range(i+1, num_pairs):
+        for k in range(j+1, num_pairs):
+            for s in range(num_serv):
+                A_ele = [0 for a in range(num_var)]
+                A_ele[i*num_serv+s] = 1
+                A_ele[j*num_serv+s] = 1
+                A_ele[k*num_serv+s] = -1
+                A.append(A_ele)
+            for s in range(num_serv):
+                A_ele = [0 for a in range(num_var)]
+                A_ele[i*num_serv+s] = 1
+                A_ele[j*num_serv+s] = -1
+                A_ele[k*num_serv+s] = 1
+                A.append(A_ele)
+            for s in range(num_serv):
+                A_ele = [0 for a in range(num_var)]
+                A_ele[i*num_serv+s] = 1
+                A_ele[j*num_serv+s] = -1
+                A_ele[k*num_serv+s] = 1
+                A.append(A_ele)
 
 b = [-g+1 for i in range(num_pairs)]
 for i in range(1, num_serv):
     b.append(0)
 for i in range(1, num_serv):
     b.append(0)
+for i in range(num_pairs):
+    for j in range(i+1, num_pairs):
+        for k in range(j+1, num_pairs):
+            for s in range(3*num_serv):
+                b.append(1)
+
+print len(A)
+print len(b)
 
 res = linprog(fnc, A_ub=A, b_ub=b, bounds=(0, 1), options={"disp": True})
 
@@ -63,7 +92,6 @@ for i in range(num_pairs):
         if round(res.x[i*num_serv+j]) == 1:
             a = conv_arr[i][0]
             b = conv_arr[i][1]
-            print "yes", a, b
             C_all[a].append(j)
             C_all[b].append(j)
 
--- xrd_extensions/lin_prog_heuristic_2.py
+++ xrd_extensions/lin_prog_heuristic_2.py
@@ -1,24 +1,29 @@
-# linear programming heuristic - variable is xij
+# linear programming heuristic 2 - variable is in form xij
 
 import random
 from scipy.optimize import linprog
 
-g = 5
+g = 3
 num_users = 10
-num_serv = 200
+num_serv = 5
 num_pairs = num_users * (num_users-1) / 2
-num_var = num_users * num_serv
+num_var = num_users * num_serv + num_pairs * num_serv
+num_var_1 = num_users * num_serv
 
-fnc = [1 for i in range(num_var)]
+fnc = [0 for i in range(num_var)]
+for i in range(num_var_1):
+    fnc[i] = 1
 
 A = []
 
+pair_index = -1
 for i in range(0, num_users):
     for j in range(i+1, num_users):
+        pair_index += 1
+
         A_ele = [0 for a in range(num_var)]
         for k in range(num_serv):
-            A_ele[i*num_serv+k] = -1
-            A_ele[j*num_serv+k] = -1
+            A_ele[num_var_1 + pair_index*num_serv+k] = -1
         A.append(A_ele) 
 
 for i in range(1, num_serv):
@@ -35,13 +40,46 @@ for i in range(1, num_serv):
         A_ele[j*num_serv+i] = -1
     A.append(A_ele)
 
-b = [2*(-g+1) for i in range(num_pairs)]
+pair_index = -1
+for i in range(0, num_users):
+    for j in range(i+1, num_users):
+        pair_index += 1
+
+        for s in range(0, num_serv):
+            A_ele = [0 for k in range(num_var)]
+            A_ele[num_var_1 + pair_index*num_serv+s] = 1
+            A_ele[i*num_serv+s] = -1
+            A.append(A_ele)
+
+        for s in range(0, num_serv):
+            A_ele = [0 for k in range(num_var)]
+            A_ele[num_var_1 + pair_index*num_serv+s] = 1
+            A_ele[j*num_serv+s] = -1
+            A.append(A_ele)
+
+        for s in range(0, num_serv):
+            A_ele = [0 for k in range(num_var)]
+            A_ele[num_var_1 + pair_index*num_serv+s] = -1
+            A_ele[i*num_serv+s] = 1
+            A_ele[j*num_serv+s] = 1
+            A.append(A_ele)
+
+b = [(-g+1) for i in range(num_pairs)]
 for i in range(1, num_serv):
     b.append(0)
 for i in range(1, num_serv):
     b.append(0)
 
+for i in range(num_pairs):
+    for j in range(num_serv):
+        b.append(0)
+    for j in range(num_serv):
+        b.append(0)
+    for j in range(num_serv):
+        b.append(1)
+
 res = linprog(fnc, A_ub=A, b_ub=b, bounds=(0, 1), options={"disp": True})
+print(res.x)
 
 ## Function to collect results
 C_all = [[] for i in range(num_users)]
@@ -66,7 +104,6 @@ def works(C_all):
                 return False
     return True
 
-
 arr = collect()
 while not works(arr):
     added_term = False
@@ -85,9 +122,6 @@ for row in arr:
     tot += len(row) 
 
 print float(tot)/len(arr)
-
-
-
 
 
 
